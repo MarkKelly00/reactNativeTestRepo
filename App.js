b959e61466539e36849e526471b297c2f2b2bce0
Mako3 <big.mako@gmail.com>
2021-03-29 01:23:12 -0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: "Mark", id: "1" },
+    { name: "Yoshi", id: "2" },
+    { name: "Mario", id: "3" },
+    { name: "Luigi", id: "4" },
+    { name: "Peach", id: "5" },
+    { name: "Toad", id: "6" },
+    { name: "Bowser", id: "7" },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+
+      {/* <ScrollView>
+        {people.map(item => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
@@ -14,8 +38,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  item: {
+    marginTop: 24,
+    marginHorizontal: 10,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
